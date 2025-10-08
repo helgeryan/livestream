5c37cb41f98b2525e5e0b581649874a4463dd0d6
Ryan Helgeson <ryanhelgeson14@gmail.com>
2025-10-08 16:34:09 -0400

    Clean up broadcast creation

--- HelgeCloudflare/API/Youtube/YoutubeService.swift
+++ HelgeCloudflare/API/Youtube/YoutubeService.swift
@@ -10,6 +10,23 @@ import Foundation
 final class YoutubeService {
     static let shared = YoutubeService()
     
+    func createNewLivestream() async throws {
+        let broadcast = try await createBroadcast()
+        let stream = try await createStream()
+        
+        print()
+        
+        let newBroadcast = try await bindBroadcast(broadcastId: broadcast.id,
+                                                   streamId: stream.id)
+        
+        print("✅ Broadcast scheduled and bound to stream!")
+        print("RTMP URL:", stream.cdn.ingestionInfo.ingestionAddress)
+        print("Stream Key:", stream.cdn.ingestionInfo.streamName)
+        StreamHelper.shared.streamKey = stream.cdn.ingestionInfo.streamName
+        StreamHelper.shared.bcId = newBroadcast.id
+        StreamHelper.shared.streamId = stream.id
+    }
+    
     func createBroadcast() async throws -> YoutubeBroadcastResponse {
         let action = YoutubeAPIAction.createBroadcast
         return try await APIManager.shared.sendRequest(action)
--- HelgeCloudflare/UI/Menu/MenuViewModel.swift
+++ HelgeCloudflare/UI/Menu/MenuViewModel.swift
@@ -12,20 +12,7 @@ class MenuViewModel {
     func scheduleYouTubeLive(accessToken: String) {
         Task {
             do {
-                let broadcast = try await YoutubeService.shared.createBroadcast()
-                let stream = try await YoutubeService.shared.createStream()
-                
-                print()
-                
-                let newBroadcast = try await YoutubeService.shared.bindBroadcast(broadcastId: broadcast.id,
-                                                                                 streamId: stream.id)
-                
-                print("✅ Broadcast scheduled and bound to stream!")
-                print("RTMP URL:", stream.cdn.ingestionInfo.ingestionAddress)
-                print("Stream Key:", stream.cdn.ingestionInfo.streamName)
-                StreamHelper.shared.streamKey = stream.cdn.ingestionInfo.streamName
-                StreamHelper.shared.bcId = broadcast.id
-                StreamHelper.shared.streamId = stream.id
+                try await YoutubeService.shared.createNewLivestream()
             } catch {
                 print(error)
             }
