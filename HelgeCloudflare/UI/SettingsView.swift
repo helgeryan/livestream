eebf865a2ec9df7b6c4e56836ab7b017b99378db
Ryan Helgeson <ryanhelgeson14@gmail.com>
2025-10-09 20:55:15 -0400

    Add a Settings page

---
+++ HelgeCloudflare/UI/SettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsView.swift
+//  HelgeCloudflare
+//
+//  Created by Ryan Helgeson on 10/9/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
