318e75002912213cb2628095f083d3012f6c4ce2
Ryan Helgeson <ryanhelgeson14@gmail.com>
2025-10-21 15:02:53 -0400

    Make a new youtube error file

--- HelgeCloudflare/API/Youtube/YoutubeService.swift
+++ HelgeCloudflare/API/Youtube/YoutubeService.swift
@@ -8,28 +8,6 @@
 import SwiftUI
 import GoogleSignIn
 
-enum YoutubeError: CustomError {
-    case noClientID
-    case noAccessToken
-    case generalError(YoutubeErrorResponse)
-    
-    
-    var title: LocalizedStringKey? {
-        return switch self {
-        case .noClientID, .noAccessToken: "Youtube Authentication Failed"
-        case .generalError: "Youtube Connection Failed"
-        }
-    }
-    
-    var userMessage: LocalizedStringKey {
-        return switch self {
-        case .noClientID: "No client ID, please contact support to resolve the issue."
-        case .noAccessToken: "Access has expired, please sign in again."
-        case .generalError(let response): LocalizedStringKey(response.error.message)
-        }
-    }
-}
-
 enum YoutubePrivacyStatus: String, CaseIterable, Identifiable, Codable {
     case `public` = "public"
     case unlisted = "unlisted"
